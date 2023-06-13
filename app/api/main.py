c908a33b668b25226fe4cbd4091ec09e83828f5a
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-06-13 12:00:58 +0500

    User API create list

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
+from api.router.amin_U import router as create_admin
 
 app = FastAPI(title="Admin",
               docs_url="/api/admin")
 
-
-@app.get("/")
-async def root():
-    return {"market": "place"}
+app.include_router(create_admin)
 
 
--- app/api/schemas/users_schemas.py
+++ app/api/schemas/users_schemas.py
@@ -2,4 +2,18 @@ from fastapi_users import schemas
 
 
 class CreateUser(schemas.BaseUserCreate):
-    pass
+    name: str
+    age: int
+    phone_number: str
+    country: str
+    region: str
+    is_staff: bool
+
+
+class ReadUser(schemas.BaseUserCreate):
+    name: str
+    age: int
+    phone_number: str
+    country: str
+    region: str
+    is_staff: bool
