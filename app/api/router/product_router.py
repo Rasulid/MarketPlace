b95ec24a9b09043537524bfec09d9f76c0c4c80b
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-06-21 16:41:10 +0500

    done product post and product list

--- app/api/router/product_router.py
+++ app/api/router/product_router.py
@@ -1,11 +1,10 @@
-import os
 import shutil
 import uuid
 from typing import List
 
-from fastapi import APIRouter, Depends, status, UploadFile, File, Body
-from api.schemas.product_schema import Product_Schema, Product_Image_Schema, \
-    AStudentWorkCreateSchema
+from fastapi import APIRouter, Depends, status, UploadFile, File
+from api.schemas.product_schema import  Product_Image_Schema, \
+    AStudentWorkCreateSchema,  Product_Schema_Read_V2
 from sqlalchemy.orm import Session, joinedload
 from api.db.session import get_db
 from api.models.product_model import Product_Model, Product_Image
@@ -17,78 +16,112 @@ router = APIRouter(
     prefix="/api/product"
 )
 
-#
-# async def upload_img(file: List[UploadFile] = File(...)):
-#     image_list = []
-#     for img in file:
-#         img.filename = f"{uuid.uuid4()}.jpg"
-#         with open(f"static/image/{img.filename}", "wb") as buffer:
-#             shutil.copyfileobj(img.file, buffer)
-#             append_for_db = Product_Image_Schema(file_name=img.filename, file_path=f"static/image")
-#         image_list.append(append_for_db)
-#     return image_list
-#
-#
-# @router.post("/create")
-# async def create_product(
-#         product: AStudentWorkCreateSchema,
-#         db: Session = Depends(get_db),
-#         file: List[UploadFile] = File(),
-#         login: dict = Depends(get_current_staff)
-# ):
-#     if login is None:
-#         return get_user_exceptions()
-#
-#     owner_id = login.get("user_id")
-#     res = []
-#     upload_image = await upload_img(file)
-#
-#     result = []
-#
-#     product_model = Product_Model()
-#     product_model.title = product.title
-#     product_model.desc = product.desc
-#     product_model.category = product.category
-#     product_model.owner = owner_id
-#     product_model.created_at = product.created_at
-#     product_model.count = product.count
-#     product_model.procent_sale = product.procent_sale
-#     product_model.promocode = product.promocode
-#     product_model.colour = product.colour
-#
-#     res.append(product_model)
-#     db.add(product_model)
-#     db.commit()
-#
-#     for x in upload_image:
-#         image_model = Product_Image()
-#         image_model.file_path = x.file_path
-#         image_model.file_name = x.file_name
-#         image_model.product_id = product_model.id
-#
-#         print(image_model.product_id)
-#         res.append(image_model)
-#
-#     res.append(result)
-#     db.add_all(result)
-#     db.commit()
-#
-#     return res
-#
-#
-# @router.get("/list-product")
-# async def product_list(db: Session = Depends(get_db),
-#                        login: dict = Depends(get_current_staff)
-#                        ):
-#     if login is None:
-#         return get_user_exceptions()
-#
-#     query = (
-#         db.query(Product_Model)
-#         .join(Product_Image, Product_Model.id == Product_Image.product_id)
-#         # .options(joinedload(Product_Model.images))
-#         .all()
-#     )
-#     print(query)
-#
-#     return query
+
+
+async def upload_img(file: List[UploadFile] = File(...)):
+    image_list = []
+    for img in file:
+        img.filename = f"{uuid.uuid4()}.jpg"
+        with open(f"static/image/{img.filename}", "wb") as buffer:
+            shutil.copyfileobj(img.file, buffer)
+            append_for_db = Product_Image_Schema(file_name=img.filename, file_path=f"static/image")
+        image_list.append(append_for_db)
+    return image_list
+
+@router.post("/create", response_model=Product_Schema_Read_V2)
+async def create_product(
+        product: AStudentWorkCreateSchema,
+        db: Session = Depends(get_db),
+        file: List[UploadFile] = File(),
+        login: dict = Depends(get_current_staff)
+):
+    if login is None:
+        return get_user_exceptions()
+
+    owner_id = login.get("user_id")
+    res = []
+    upload_image = await upload_img(file)
+
+    result = []
+
+    product_model = Product_Model()
+    product_model.title = product.title
+    product_model.desc = product.desc
+    product_model.category = product.category
+    product_model.owner = owner_id
+    product_model.created_at = product.created_at
+    product_model.count = product.count
+    product_model.procent_sale = product.procent_sale
+    product_model.promocode = product.promocode
+    product_model.colour = product.colour
+
+    db.add(product_model)
+    db.commit()
+    res.append(product_model)
+
+    for x in upload_image:
+        image_model = Product_Image()
+        image_model.file_path = x.file_path
+        image_model.file_name = x.file_name
+        image_model.product_id = product_model.id
+
+        result.append(image_model)
+
+    db.add_all(result)
+    db.commit()
+
+    images_data = []
+    for image in result:
+        image_data = Product_Image_Schema(file_name=image.file_name,
+                                          file_path=image.file_path)
+        images_data.append(image_data)
+
+    product_data = Product_Schema_Read_V2(
+        title=product.title,
+        desc=product.desc,
+        category=product.category,
+        images=images_data,
+        owner=product_model.owner,
+        created_at=product_model.created_at,
+        count=product_model.count,
+        procent_sale=product_model.procent_sale,
+        promocode=product_model.promocode,
+        colour=product_model.colour,
+    )
+
+    return product_data
+
+@router.get("/list-product", response_model=List[Product_Schema_Read_V2])
+async def product_list(db: Session = Depends(get_db),
+                       login: dict = Depends(get_current_staff)):
+    if login is None:
+        return get_user_exceptions()
+
+    query = (
+        db.query(Product_Model)
+        .join(Product_Image, Product_Model.id == Product_Image.product_id)
+        .options(joinedload(Product_Model.images))
+        .all()
+    )
+    products = []
+    for product in query:
+        images = [
+            Product_Image_Schema(file_name=image.file_name, file_path=image.file_path)
+                  for image in product.images]
+
+
+        product_data = Product_Schema_Read_V2(
+            title=product.title,
+            desc=product.desc,
+            category=product.category,
+            owner=product.owner,
+            created_at=product.created_at,
+            count=product.count,
+            procent_sale=product.procent_sale,
+            promocode=product.promocode,
+            colour=product.colour,
+            images=images
+        )
+        products.append(product_data)
+
+    return products
--- app/api/schemas/product_schema.py
+++ app/api/schemas/product_schema.py
@@ -1,5 +1,62 @@
+# import json
+# from datetime import datetime
+# from typing import List, Optional
+# from pydantic import BaseModel
+#
+#
+# class Product_Image_Schema(BaseModel):
+#     file_name: str
+#     file_path: str
+#
+#
+# class Product_Schema_Read(BaseModel):
+#     title: str
+#     desc: str
+#     category: str
+#     owner: int
+#     created_at: datetime
+#     count: int
+#     procent_sale: Optional[int]
+#     promocode: Optional[str]
+#     colour: str
+#     images: List[Product_Image_Schema]
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class Product_Schema(BaseModel):
+#     title: str
+#     desc: str
+#     category: str
+#     created_at: datetime
+#     count: int
+#     procent_sale: Optional[int]
+#     promocode: Optional[str]
+#     colour: str
+#
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class AStudentWorkCreateSchema(Product_Schema):
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls.validate_to_json
+#
+#     @classmethod
+#     def validate_to_json(cls, value):
+#         if isinstance(value, str):
+#             return cls(**json.loads(value))
+#         return value
+
+
 import json
 from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
 from typing import List, Optional
 from pydantic import BaseModel
 
@@ -31,10 +88,25 @@ class Product_Schema(BaseModel):
     category: str
     created_at: datetime
     count: int
+    procent_sale: int
+    promocode: str
     procent_sale: Optional[int]
     promocode: Optional[str]
     colour: str
 
+
+class Product_Schema_Read_V2(BaseModel):
+
+    title: str  # Название продукта
+    desc: str  # Описание продукта
+    category: str  # Категория продукта
+    images: List[Product_Image_Schema]  # Список изображений продукта
+    owner: int  # Идентификатор владельца продукта
+    created_at: datetime  # Дата и время создания продукта
+    count: int  # Количество продукта (целое число)
+    procent_sale: int  # Процент скидки на продукт (целое число)
+    promocode: str  # Промокод продукта
+    colour: str  # Цвет продукта
 
     class Config:
         orm_mode = True
@@ -44,7 +116,6 @@ class AStudentWorkCreateSchema(Product_Schema):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate_to_json
-
     @classmethod
     def validate_to_json(cls, value):
         if isinstance(value, str):
