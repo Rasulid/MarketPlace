e577fc715c34d0d896a8e1c75be243ddf7b70585
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-06-27 09:49:24 +0500

    add relationship to ordered products

--- app/api/models/order_model.py
+++ app/api/models/order_model.py
@@ -3,6 +3,13 @@ from sqlalchemy.orm import relationship
 from api.db.DataBasse import Base
 
 
+class OrderedProduct(Base):
+    __tablename__ = 'ordered_products'
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.id', ondelete="CASCADE"))
+    order_id = Column(Integer, ForeignKey('orders.id', ondelete="CASCADE"))
+    count = Column(Integer)
+
 
 class Order(Base):
     __tablename__ = 'orders'
@@ -10,16 +17,5 @@ class Order(Base):
     payment_method = Column(String)
     total_price = Column(Float)
     order_status = Column(String)
-    # user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
-    # ordered_products = relationship("OrderedProduct", back_populates="order")
-
-
-class OrderedProduct(Base):
-    __tablename__ = 'ordered_products'
-    id = Column(Integer, primary_key=True)
-    # product_id = Column(Integer, ForeignKey('products.id', ondelete="CASCADE"))
-    # order_id = Column(Integer, ForeignKey('orders.id', ondelete="CASCADE"))
-    count = Column(Integer)
-    # product = relationship("Product", back_populates="ordered_products")
-    # order = relationship("Order", back_populates="ordered_products")
-    #
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    ordered_products = relationship("OrderedProduct", secondary=OrderedProduct)
