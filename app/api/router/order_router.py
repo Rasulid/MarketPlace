e4e6508ed7f0f9b800d8bfc2c8a8deebd29232d2
rasulabduvaitov@gmail.com <rasulabduvaitov@gmail.com>
2023-06-29 17:47:54 +0500

    orders list is done

--- app/api/router/order_router.py
+++ app/api/router/order_router.py
@@ -14,51 +14,7 @@ router = APIRouter(tags=['Orders'],
                    prefix="/api/orders")
 
 
-@router.post('/create', response_model=List[OrderSchemaRead])
-async def create_order(user_id: int,
-                       product_id: int,
-                       order_schema: OrderSchema,
-                       ordered_product_schema: List[OrderedProductSchema],
-                       db: Session = Depends(get_db)):
-    query_to_prod = db.query(ProductModel) \
-        .filter(ProductModel.id == product_id) \
-        .first()
-
-    query_to_user = db.query(UserModel) \
-        .filter(UserModel.id == user_id) \
-        .first()
-
-    if query_to_prod is None or query_to_user is None:
-        return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content="Not Found")
-
-    order = Order()
-    order.payment_method = order_schema.payment_method
-    order.total_price = query_to_prod.price
-    order.order_status = order_schema.order_status
-    order.user_id = user_id
-    order.count = order_schema.count
-    # order.ordered_products = ordered_prod
-
-    order_count = order.count
-
-    # db.add(order)
-    # db.commit()
-    for product in range(order_count):
-        count = 1
-        ordered_prod = OrderedProduct()
-        ordered_prod.product_id = query_to_prod.id
-        ordered_prod.order_id = order.id
-
-        count += 1
-        # db.add(ordered_prod)
-        # db.commit()
-
-    return order
-
-
-@router.post('/create_test', response_model=OrderSchemaRead)
+@router.post('/create', response_model=OrderSchemaRead)
 async def create_order(order_schema: OrderSchema,
                        product_id: int,
                        user_id: int,
@@ -114,3 +70,44 @@ async def create_order(order_schema: OrderSchema,
     )
 
     return response
+
+
+@router.get('/orders/{id}', response_model=List[OrderSchemaRead])
+async def order_list(id: int, db: Session = Depends(get_db)):
+    query = (
+        db.query(Order, OrderedProduct)
+        .join(OrderedProduct, Order.id == OrderedProduct.order_id)
+        .filter(Order.user_id == id)
+        .all()
+    )
+
+    orders_with_products = []
+    current_order = None
+    current_ordered_products = []
+    for order, ordered_product in query:
+        if current_order is None or current_order.id != order.id:
+            if current_order is not None:
+                orders_with_products.append(OrderSchemaRead(
+                    payment_method=current_order.payment_method,
+                    total_price=current_order.total_price,
+                    order_status=current_order.order_status,
+                    user_id=current_order.user_id,
+                    order_products=current_ordered_products
+                ))
+            current_order = order
+            current_ordered_products = []
+        current_ordered_products.append(OrderedProductSchema(
+            product_id=ordered_product.product_id,
+            order_id=ordered_product.order_id
+        ))
+
+    if current_order is not None:
+        orders_with_products.append(OrderSchemaRead(
+            payment_method=current_order.payment_method,
+            total_price=current_order.total_price,
+            order_status=current_order.order_status,
+            user_id=current_order.user_id,
+            order_products=current_ordered_products
+        ))
+
+    return orders_with_products
